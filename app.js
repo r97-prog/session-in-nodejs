caefe933bc5f496c72758919e169459e12b16197
ravi.mali <ravi.mali@pixelnx.com>
2021-08-06 19:12:24 +0530

    first session in node js

---
+++ app.js
@@ -0,0 +1,84 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session')
+var mongoose = require('mongoose')
+var mongoDBSession = require("connect-mongodb-session")(session);
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// DB connection 
+
+const mongoURI = "mongodb+srv://rishabh:gorzR3libdIegmmh@cluster0.cvk4k.mongodb.net/rishabh"
+
+const store = new mongoDBSession({
+  uri:mongoURI,
+  collection:"mySessions",
+});
+
+mongoose.connect(mongoURI,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("connection successfully")
+}).catch((error)=>{
+    console.log("Connection Error " +error)
+});
+
+
+
+
+// session middleware
+app.use(session({
+  secret:"key that will sign cookie",
+  resave:"false",
+  saveUninitialized:false,
+  store:store,
+}))
+
+// session request
+
+app.get("/session",(req,res)=>{
+  req.session.isAuth = true;
+  // console.log(req.session);
+  // console.log(req.session.id);
+  res.send("Hello from session");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
